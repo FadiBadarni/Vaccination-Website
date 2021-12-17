ffb47883f1f819f07d707c37140fd2dc3def4626
fadi badarni <fadybd1@gmail.com>
2021-12-17 15:15:35 +0200

    Added base.html , other html files now take from it

--- users/views.py
+++ users/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+    # check if request is of type post (user register)
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+
+        # check if form received is valid
+        if form.is_valid():
+            form.save()     # Save the user
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Registered For {username} .')       # Flash message usage with f_string
+            return redirect('login')   # Redirect user to home page
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})  # Render register with form
+
+
+@login_required    # Decorator to add restrictions to profile view
+def profile(request):
+    return render(request, 'users/profile.html')
+
+
+
+
+
