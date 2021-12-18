4a5eeac86b1d57aa2ca9d4cc7c8cb70c872a1304
fadi badarni <fadybd1@gmail.com>
2021-12-18 11:29:47 +0200

    Added Design For home page + Email authentication + password reset and confirmation email ..

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,3 +131,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'Web-Home'
 
 LOGIN_URL = 'login'   # For use with decorator login_required
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'covaccinesce@gmail.com'
+EMAIL_HOST_PASSWORD = 'fade0524183083'
+
+
