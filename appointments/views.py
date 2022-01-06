bd9e009290194d0244bc84594bd2a61e5280bdc2
Abed-alrahman1 <93323471+Abed-alrahman1@users.noreply.github.com>
2022-01-06 23:20:15 +0200

    update workings day

--- appointments/views.py
+++ appointments/views.py
@@ -6,4 +6,14 @@ def appointment(request):
     return render(request, 'appointments/reservation.html')
 
 
-
+@allowed_users(allowed_roles=['doctor'])
+def my_working_days(request):
+    working_days = []
+    holidays = []
+    for day in WEEK_DAYS:
+        if day[0] == request.user.profile.holiday:
+            holidays.append(day[1])
+        else:
+            working_days.append(day[1])
+    context = {'working_days': working_days, 'holidays': holidays}
+    return render(request, 'appointments/working-days.html', context)
--- authentication/urls.py
+++ authentication/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('about/', views.about, name='About-Page'),
     path('FAQ/', views.faq, name='FAQ-Page'),
     path('contact/', views.contact, name='Contact-Page'),
+    path('my_working_days/', views.my_working_days, name='my_working_days'),
+    path('manage_working_days/', views.manage_working_days, name='manage_working_days'),
 
 ]
 
