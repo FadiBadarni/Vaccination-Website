d21f0cb5de23af5f22bd6cbdaf4f880a4399dee6
Abed-alrahman1 <93323471+Abed-alrahman1@users.noreply.github.com>
2022-01-06 23:49:26 +0200

    seeing my appointment

--- appointments/views.py
+++ appointments/views.py
@@ -5,6 +5,37 @@ from django.http import HttpResponse
 def appointment(request):
     return render(request, 'appointments/reservation.html')
 
+def my_appointments(request):
+    return render(request, 'appointments/my-appointments.html')
+
+@user_not_confined
+def book_appointment(request):
+    day = request.GET.get("day", None)
+    time = request.GET.get("time", None)
+    hour = time.split(":")[0]
+    minute = time.split(":")[1]
+    time = datetime.time(hour=int(hour), minute=int(minute))
+    if Appointment.is_time_available(time, day):
+        Appointment.objects.create(
+            user = request.user,
+            time = time,
+            day = day,
+        )
+        messages.success(request, f"Appointment Booked Successfully For {time}.")
+    else:
+        messages.error(request, f"Appointment Already Booked.")
+    return redirect('appointment')
+
+def unbook_appointment(request):
+    appointment_id = request.GET.get("ap_id", None)
+    appointment = get_object_or_404(Appointment, id = appointment_id)
+    if appointment.user == request.user:
+        appointment.delete()
+        messages.success(request, f"Appointment UnBooked Successfully.")
+    else:
+        return HttpResponseForbidden()
+    return redirect('appointment')
+
 
 @allowed_users(allowed_roles=['doctor'])
 def my_working_days(request):
