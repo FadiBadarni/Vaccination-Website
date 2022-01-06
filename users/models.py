1b67d885d92ba1276dae3b13cf11d68560b9b289
mohamd <mhmd.abood.2001@gmail.com>
2022-01-07 00:48:50 +0200

    workking days,doctor vaccient, accepted vaccinet,sign in for doctor

--- users/models.py
+++ users/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+WEEK_DAYS = (
+    (1, 'Monday'),
+    (2, 'Tuesday'),
+    (3, 'Wednesday'),
+    (4, 'Thursday'),
+    (7, 'Sunday'),
+)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    image = models.ImageField(default='profile_pics/default.png', upload_to='profile_pics/')
+    holiday = models.IntegerField(choices=WEEK_DAYS, default=7, null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+    def get_image_url(self):
+        try:
+            return self.image.url
+        except:
+            return ''
+
+    def get_appointments(self):
+        try:
+            return self.user.appointments.all().order_by('-date_created')
+        except:
+            return None
+
+
+from .signals import *
